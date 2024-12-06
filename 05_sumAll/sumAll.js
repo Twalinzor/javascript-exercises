d2984a0224bcc73614acfbc1139323dc939a88e1
Twalinzor <stanislautapchylka@gmail.com>
2024-12-06 04:02:20 +0100

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (num, num2) {
+  let result = 0;
+  if (num2 > num) {
+    for (let i = num; i <= num2; i++) {
+      result += i;
+    }
+  } else {
+    for (let i = num2; i <= num; i++) {
+      result += i;
+    }
+  }
+  if (
+    num < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num) ||
+    !Number.isInteger(num2) ||
+    isNaN(num, num2)
+  ) {
+    return "ERROR";
+  } else return result;
 };
 
 // Do not edit below this line
