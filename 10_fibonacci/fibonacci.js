c40e392e10a8d370b2d47fe93654a15b72c4fad2
Twalinzor <stanislautapchylka@gmail.com>
2024-12-10 05:05:42 +0100

    lol

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = (num) => {
+  const parsedNum = parseInt(num, 10);
+  if (isNaN(parsedNum) || parsedNum < 0) return "OOPS";
+  const fib = [1, 1];
+  for (let i = 2; i < num; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  if (num > 0) {
+    return fib[num - 1];
+  } else return 0;
 };
 
 // Do not edit below this line
